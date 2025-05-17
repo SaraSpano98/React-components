c14e5d0629f21c9ac5ad5a5387e42d6ec5740e73
Sara Spano <saraspano@live.com>
2025-05-17 11:04:50 +0200

    added FabButton component

---
+++ Buttons/FabButton/src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { FabButton } from './FabButton.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <FabButton />
+)
